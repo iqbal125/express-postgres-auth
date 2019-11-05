54fb4097e43131747a40009355a78be65389bb65
iqbal125 <iqbal125@yahoo.com>
2019-11-05 11:49:37 -0800

    added jwt id lookup and minor changes

--- Authentication/passport_config.js
+++ Authentication/passport_config.js
@@ -1,108 +1,103 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const ExtractJWT = require("passport-jwt").ExtractJwt;
-const JWTStrategy   = require("passport-jwt").Strategy;
-const db = require('../db.js')
-const checkPassword = require('./helpers').checkPassword
-const setToken = require('./helpers').setToken
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const JWTStrategy = require('passport-jwt').Strategy;
+const db = require('../db.js');
+const checkPassword = require('./helpers').checkPassword;
+const setToken = require('./helpers').setToken;
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 
 const localSignIn = (req, res) => {
-    oPts = {
-        session: false,
-    }
-    passport.authenticate('local', oPts, (error, user, info) => {
-        if(error) { res.status(500).send(error); }
-        if(info) { res.status(401).send(info); }
-        if(!user && !info) { 
-            res.status(401).send('Authentication Failed');
-        } 
-        if(user) {
-            user = {
-                id: user.rows[0].id,
-                username: user.rows[0].username
-            }
-            //send jwt token as login
-            res.send({token: setToken(user)});
-        }
-   })(req, res)
-}
+	oPts = {
+		session: false,
+	};
+	passport.authenticate('local', oPts, (error, user, info) => {
+		if (error) {
+			res.status(500).send(error);
+		}
+		if (info) {
+			res.status(401).send(info);
+		}
+		if (!user && !info) {
+			res.status(401).send('Authentication Failed');
+		}
+		if (user) {
+			user = {
+				id: user.rows[0].id,
+				username: user.rows[0].username,
+			};
+			//send jwt token as login
+			res.send({ token: setToken(user) });
+		}
+	})(req, res);
+};
 
+passport.use(
+	new JWTStrategy(
+		{
+			jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+			secretOrKey: 'secret',
+		},
+		(jwtPayload, cb) => {
+			let id = [jwtPayload.id];
+			let query = `SELECT *
+                   FROM users 
+                   WHERE id=$1`;
 
-passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey   : 'secret'
-    },
-  (jwtPayload, cb) => {
-    //check if id exists in database
-        return cb(null, jwtPayload)
-    }
-));
+			//check if id exists then allow endpoint
+			let callback = (err, user) => {
+				if (err) {
+					return cb(err, false);
+				}
+				if (user.rows.length === 0) {
+					return cb(null, false);
+				}
+				if (user.rows.length != 0) {
+					return cb(null, jwtPayload);
+				}
+			};
+			db.query(query, id, callback);
+		}
+	)
+);
 
+passport.use(
+	new LocalStrategy(
+		{
+			//change if your property names are different
+			usernameField: 'username',
+			passwordField: 'password',
+			session: false,
+		},
+		(username, password, done) => {
+			let query = `SELECT * from users
+                 WHERE username=$1`;
 
-passport.use(new LocalStrategy({
-    //change if your property names are different
-    usernameField: 'username',
-    passwordField: 'password',
-    session: false
-  }, (username, password, done) => {
-  
-    let query = `SELECT * from users
-                 WHERE username=$1`
-  
-    let values = [username]
+			let values = [username];
 
-    //check if username exists and then check password
-    let callback = (err, user) => {
-      if (err) { return console.log(err); }
-      if (user.rows.length === 0) {
-          return done(null, false, {message: "User Not Found"} ) 
-        }
-      if(user.rows.length != 0) {
-        checkPassword(password, user.rows[0].password, done)
-          .then(() => done(null, user))
-          .catch(err => console.log(err))
-      }
-    }
-    db.query(query, values, callback) 
-}))
-
-
-
-// uncomment if  you want sessions
-// passport.serializeUser((user, done) => {
-//   console.log(user)
-//   if(user.rows) {
-//     done(null, user.rows[0].id)
-//   }
-//   else {
-//     done(null, user.id)
-//   }
-// })
-
-// passport.deserializeUser((id, cb) => {
-//   let query = `SELECT * FROM users
-//                WHERE id = $1`
-  
-//   let values = [id]
-
-//   let callback = (err, results) => {
-//     if(err) {
-//       console.log(err)
-//     }
-//     console.log(err)
-//     cb(null, results.rows[0])
-//   }
-
-//   db.query(query, values, callback)
-// })
-
+			//check if username exists and then check password
+			let callback = (err, user) => {
+				if (err) {
+					return console.log(err);
+				}
+				if (user.rows.length === 0) {
+					return done(null, false, { message: 'User Not Found' });
+				}
+				if (user.rows.length != 0) {
+					checkPassword(password, user.rows[0].password, done)
+						.then(() => done(null, user))
+						.catch(err => console.log(err));
+				}
+			};
+			db.query(query, values, callback);
+		}
+	)
+);
 
 const exportObj = {
-    requireAuth, 
-    localSignIn
-}
+	requireAuth,
+	localSignIn,
+};
 
-module.exports = exportObj
+module.exports = exportObj;
--- db.js
+++ db.js
@@ -1,11 +1,14 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
+const { db_pwd } = require('./keys');
+
+console.log(db_pwd);
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'newdb',
-  password: '',
-  post: 5432
-})
+	user: 'postgres',
+	host: 'localhost',
+	database: 'newdb',
+	password: db_pwd,
+	post: 5432,
+});
 
-module.exports = pool
+module.exports = pool;
