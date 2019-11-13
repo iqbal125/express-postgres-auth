70f959baa75337ce6c64f1d9ad5dfb362239422a
iqbal125 <iqbal125@yahoo.com>
2019-11-13 15:41:04 -0800

    added oauth2 support

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-const { Pool } = require('pg');
-const { db_pwd } = require('./keys');
-
-console.log(db_pwd);
+const { Pool } = require("pg");
+const { db_pwd } = require("./keys");
 
 const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'newdb',
-	password: db_pwd,
-	post: 5432,
+  user: "postgres",
+  host: "localhost",
+  database: "newdb",
+  password: db_pwd,
+  post: 5432
 });
 
 module.exports = pool;
