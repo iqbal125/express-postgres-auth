8d4ba2293480c84bc9b4d44a3c2fd3a237afd18a
iqbal125 <iqbal125@yahoo.com>
2019-11-13 15:42:52 -0800

    v.09

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 var express = require('express');
 var cors = require('cors');
-var bodyParser = require('body-parser')
-const passport = require('passport')
+var bodyParser = require('body-parser');
+const passport = require('passport');
 const sgMail = require('@sendgrid/mail');
-const auth = require('./Authentication/auth_routes') 
+const auth = require('./Authentication/auth_routes');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
 
-
-//Middlewares
+//Middleware
 app.use(cors());
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 //uncomment if want to use sessions
 // app.use(passport.session())
-app.use('/', auth)
+app.use('/', auth);
 
-//server setup 
+//server setup
 const port = process.env.PORT || 3000;
 
 app.listen(port);
