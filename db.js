6e98174c08cc97f3e52e606bb8278c0ed35e1e1d
iqbal125 <iqbal125@yahoo.com>
2020-08-01 17:12:48 -0700

    update database config

--- db.js
+++ db.js
@@ -1,12 +1,22 @@
-const { Pool } = require("pg");
-const { db_pwd } = require("./keys");
+const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "newdb",
-  password: db_pwd,
-  post: 5432
+// Meant for heroku db env var
+let connectionString =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DATABASE_URL
+    : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+let pool = new Pool({
+  connectionString
 });
+
+// Alternate Syntax
+// let pool = new Pool({
+//  user: process.env.DB_USER,
+//  host: process.env.DB_HOST,
+//  database: process.env.DB_NAME,
+//  password: process.env.DB_PASSWORD,
+//  port: process.env.DB_PORT
+// })
 
 module.exports = pool;
--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 
 
 CREATE TABLE users (
-  id PRIMARY SERIAL KEY,
+  id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE,
   password VARCHAR(255),
   email VARCHAR(255),
   resettoken VARCHAR(255),
   resettokentime VARCHAR(255)
 );
+
